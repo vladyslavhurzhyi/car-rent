6fa1fefe2b0b1e09b6eeb3dc4aa9a3cb3ac949f5
vladyslavhurzhyi <soulline0@gmail.com>
2023-10-30 10:55:35 +0100

    fix mob style for DealOfTheDay section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "09f75404c8f12fbe"; }
+/******/ 		__webpack_require__.h = function() { return "c1f4c38e5459fc81"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
